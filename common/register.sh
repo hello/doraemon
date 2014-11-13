7c63c4d10a8fd25082a9d2f2138b9c1ee255d8da
Jackson Chen <jchen@Jacksons-MacBook-Pro.local>
2014-11-12 17:12:23 -0800

    encrypt at the end

--- common/register.sh
+++ common/register.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 set -e
 echo "Uploading factory file"
+key="nordic_key_public.pem"
 get_id(){
     xxd -s 0x60 -l 8 -g 8 -u -ps $1
 }
 chmod 600 $DEVICE_INFO_IMG
 if [ -s $DEVICE_INFO_IMG ]; then
-    mv -f $DEVICE_INFO_IMG $REGISTRATIONDIR/$(get_id $DEVICE_INFO_IMG)
+    fname=$(get_id $DEVICE_INFO_IMG)
+    encfname="$REGISTRATIONDIR/${fname}.encrypted"
+    $COMMONDIR/encrypt.sh "$DEVICE_INFO_IMG" "$encfname" "$COMMONDIR/$key" \
+        && rm -f "$DEVICE_INFO_IMG"
 else
     echo "Removing empty info"
     rm -f $DEVICE_INFO_IMG
