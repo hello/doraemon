0099d8d0a68e340db218e9cf57fa3050970de07d
Jackson Chen <jchen@hello.is>
2014-11-13 14:39:23 -0800

    Added preamble script

---
+++ mid.bat
@@ -0,0 +1,64 @@
+::CHANGE THIS!!
+SET COM=5
+
+
+SET PWD=%~dp0
+SET TARG=%1
+if not "%~2"=="" (
+	echo "Please supply a target name"
+	goto :fail
+)
+SET TEMPDIR=%PWD%temp
+IF NOT EXIST  %TEMPDIR% (
+	echo "creating temporary directory"
+	mkdir %PWD%temp
+)
+
+SET TARGFOLDER=%PWD%targets\%TARG%
+IF NOT EXIST  %TARGFOLDER% (
+	echo "target directory does not exist"
+	goto :fail
+)
+
+SET TARGMIDSRC=%TARGFOLDER%\kitsune.bin
+SET TARGTOPSRC=%TARGFOLDER%\morpheus+*.bin
+SET TARGHWSRC=%TARGFOLDER%\hw_ver.txt
+
+IF NOT EXIST %TARGMIDSRC% (
+	echo "midboard binary does not exist"
+	goto :fail
+)
+IF NOT EXIST %TARGTOPSRC% (
+	echo "topboard binary does not exist"
+	goto :fail
+)
+IF NOT EXIST %TARGHWSRC% (
+	echo "hardware version does not exist"
+	goto :fail
+)
+
+SET TARGMIDDST=%TEMPDIR%\kitsune.bin
+SET TARGTOPDST=%TEMPDIR%\factory.bin
+SET TARGHWDST=%TEMPDIR%\hw_ver.txt
+
+IF NOT EXIST %TARGMIDDST% (
+	echo "Copying kitsune"
+	xcopy /y /v     "%TARGMIDSRC%" "%TARGMIDDST%"
+)
+IF NOT EXIST %TARGTOPDST% (
+	echo "Copying Top Factory"
+	xcopy /y /v    "%TARGTOPSRC%" "%TARGTOPDST%"
+)
+IF NOT EXIST %TARGHWDST% (
+	echo "Copying hw version"
+	xcopy /y /v    "%TARGHWSRC%"  "%TARGHWDST%"
+)
+
+
+CALL %PWD%Uniflash\flashit.bat %COM%
+goto :eof
+
+:fail
+	echo "=========FAIL==========="
+	goto :eof
+:eof
