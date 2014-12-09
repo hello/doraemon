dd02e9252b7c3383428ea19c6748e2366e81f1c1
Jackson Chen <jchen@hello.is>
2014-12-09 14:42:32 +0800

    Added top.bat

--- top.bat
+++ top.bat
@@ -3,20 +3,42 @@ cls
 SET PWD=%~dp0
 SET TARG=%1
 SET CYGWIN=%PWD%misc\bin\win64\
-SET TEMPDIR=%PWD%temp
+SET TEMPDIR=%PWD%cached/%TARG%\
+SET JLINK=%PWD%JLinkWin/JLink.exe
+SET DEVICEROOT=%PWD%devices\
+SET DEVICEDIR=%DEVICEROOT%%TARG%
 
-IF NOT EXIST %TEMPDIR% (
-	echo "Creating temporary directory"
-	mkdir %PWD%temp
-)
-
-SET TARGFOLDER=%PWD%targets\%TARG%
-IF NOT EXIST %TARGFOLDER% (
+IF NOT EXIST  %TEMPDIR% (
 	echo "target directory does not exist"
 	goto :fail
 )
 
+IF NOT EXIST  %DEVICEROOT% (
+	echo "device root directory does not exist, creating"
+	mkdir %DEVICEROOT%
+)
+IF NOT EXIST  %DEVICEDIR% (
+	echo "device directory does not exist, creating"
+	mkdir %DEVICEDIR%
+)
 
+REM %JLINK% -device nrf51422 -if swd -speed 4000 -CommanderScript %TEMPDIR%flash.jlink
+
+SET DEVICEINFO=%TEMPDIR%device.info
+
+for /f %%i in ("%DEVICEINFO%") do set "size=%%~zi"
+if not defined size set size=0
+if %size% gtr 0 (
+    echo file found
+) else (
+    echo  Unable to readback
+    goto :fail
+)
+
+
+FOR /F "tokens=1 delims=\\ " %%i in ('%CYGWIN%/sha1sum.exe %DEVICEINFO%') do SET SHA=%%i
+
+%CYGWIN%mv %DEVICEINFO% %DEVICEDIR%/%SHA%
 
 
 	echo "****************************************************"
@@ -24,6 +46,7 @@ IF NOT EXIST %TARGFOLDER% (
 	echo "*                       PASS                       *"
 	echo "*                                                  *"
 	echo "****************************************************"
+goto :eof
 :fail
 	echo "****************************************************"
 	echo "*                                                  *"
