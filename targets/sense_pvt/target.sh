c4fff211bde824c37a043935a96381e1f53a899e
Jackson Chen <jchen@Jacksons-MacBook-Pro.local>
2015-01-07 17:35:56 -0800

    Added PVT

---
+++ targets/sense_pvt/target.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+SD_MAIN_IMG=s310_nrf51422_1.0.0_softdevice_main.bin
+UICR_MAIN_IMG=s310_nrf51422_1.0.0_softdevice_uicr.bin
+JLINK_TEMPLATE=app+bootloader.prod.in
+BOOTLOADER_IMG="$(basename $(ls $TARGETDIR/bootloader*))"
+APP_IMG="$(basename $(ls $TARGETDIR/morpheus*))"
+CRC_IMG="$(basename $(ls $TARGETDIR/*.crc))"
+
+cp_to_work(){
+    cp -f -v $TARGETDIR/$1 $WORKDIR/$1
+}
+
+cp_to_work $SD_MAIN_IMG
+cp_to_work $UICR_MAIN_IMG
+cp_to_work $APP_IMG
+cp_to_work $BOOTLOADER_IMG
+cp_to_work $CRC_IMG
+sed\
+    -e "s,\$SOFTDEVICE_MAIN,$WORKDIR/$SD_MAIN_IMG,g"\
+    -e "s,\$SOFTDEVICE_UICR,$WORKDIR/$UICR_MAIN_IMG,g"\
+    -e "s,\$APP,$WORKDIR/$APP_IMG,g"\
+    -e "s,\$BOOTLOADER,$WORKDIR/$BOOTLOADER_IMG,g"\
+    -e "s,\$CRC,$WORKDIR/$CRC_IMG,g"\
+    -e "s,\$DEVICEINFO,$DEVICE_INFO_IMG,g"\
+    <$TARGETDIR/$JLINK_TEMPLATE > $WORKDIR/flash.jlink
+
