36fd3d441679e331f74da2ee2626bb4cd5eff7e1
Chris Johnson <plasticchris@gmail.com>
2015-01-09 10:28:06 -0800

    New SL SDK/Service pack

--- Uniflash/flashit.bat
+++ Uniflash/flashit.bat
@@ -1,1 +1,1 @@
-%~dp0uniflashcli.bat -config "%~dp0uniflash_session\uniflash.ucf" -setOptions com=%1 secure=false spPath="%~dp0servicepack_1.0.0.1.2.bin" -operations format servicePackUpdate program
+%~dp0uniflashcli.bat -config "%~dp0uniflash_session\uniflash.ucf" -setOptions com=%1 secure=false spPath="%~dp0SP1.02_2.3.7.8.1.2.4.2.1.5.3.26.bin" -operations format servicePackUpdate program
